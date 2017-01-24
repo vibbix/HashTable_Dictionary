39af649f21230a08f0a368cbdaee93517970a817
end user <kreimendahlf@wit.edu>
2017-01-24 11:06:57 -0500

    added skeleton code

---
+++ src/edu/wit/cs/comp2350/SimpleTable.java
@@ -0,0 +1,17 @@
+package edu.wit.cs.comp2350;
+
+public class SimpleTable extends HashTable {
+		
+	public SimpleTable(int size) {
+		super(size);
+	}
+
+	@Override
+	public int calculateHash(String word) {
+		// TODO: implement hash from slides that uses multiplier
+		int hash = 0;
+
+		return hash;
+	}
+
+}
