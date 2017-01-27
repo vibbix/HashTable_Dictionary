a74b2e59e4db52b66cd88b96caec018a124f3839
Mark Beznos <beznosm@wit.edu>
2017-01-26 21:10:29 -0500

    Implemented rand & simple table hash methods Added math.abs to hash return values to prevent negative array bound values

--- src/edu/wit/cs/comp2350/SimpleTable.java
+++ src/edu/wit/cs/comp2350/SimpleTable.java
@@ -1,17 +1,21 @@
 package edu.wit.cs.comp2350;
 
 public class SimpleTable extends HashTable {
-		
-	public SimpleTable(int size) {
-		super(size);
-	}
 
-	@Override
-	public int calculateHash(String word) {
-		// TODO: implement hash from slides that uses multiplier
-		int hash = 0;
+    private static final int MULTIPLIER = 31;
 
-		return hash;
-	}
+    public SimpleTable(int size) {
+        super(size);
+    }
+
+    @Override
+    public int calculateHash(String word) {
+        byte[] bytes = word.getBytes();
+        int hash = 0;
+        for (byte b : bytes) {
+            hash = ((hash * MULTIPLIER) + b) % this.tableSize;
+        }
+        return Math.abs(hash);
+    }
 
 }
--- src/edu/wit/cs/comp2350/tests/ChartMaker.java
+++ src/edu/wit/cs/comp2350/tests/ChartMaker.java
@@ -1,10 +1,10 @@
 package edu.wit.cs.comp2350.tests;
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
+import edu.wit.cs.comp2350.BadTable;
+import edu.wit.cs.comp2350.HashTable;
+import edu.wit.cs.comp2350.RandTable;
+import edu.wit.cs.comp2350.SimpleTable;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
@@ -19,97 +19,94 @@ import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
 
-import edu.wit.cs.comp2350.BadTable;
-import edu.wit.cs.comp2350.HashTable;
-import edu.wit.cs.comp2350.RandTable;
-import edu.wit.cs.comp2350.SimpleTable;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class ChartMaker extends ApplicationFrame
-{
-	private static JFreeChart createClusteredChart(String title, String categoryAxisLabel, String valueAxisLabel, IntervalXYDataset dataset) {
+public class ChartMaker extends ApplicationFrame {
+    private static final long serialVersionUID = 1L;
 
-		NumberAxis domainAxis = new NumberAxis(categoryAxisLabel);
-		domainAxis.setAutoRangeIncludesZero(false);
+    public ChartMaker(String chartTitle) {
+        super("Hashtable Chart");
+        JFreeChart barChart = createClusteredChart(
+                chartTitle,
+                "Index",
+                "Count",
+                createDataset());
 
-		ValueAxis valueAxis = new NumberAxis(valueAxisLabel);
+        ChartPanel chartPanel = new ChartPanel(barChart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
+        setContentPane(chartPanel);
+        XYPlot plot = (XYPlot) barChart.getPlot();
+        XYBarRenderer renderer = (XYBarRenderer) plot.getRenderer();
+        renderer.setShadowVisible(false);
+    }
 
-		XYBarRenderer renderer = new ClusteredXYBarRenderer();
+    private static JFreeChart createClusteredChart(String title, String categoryAxisLabel, String valueAxisLabel, IntervalXYDataset dataset) {
 
-		XYPlot plot = new XYPlot(dataset, domainAxis, valueAxis, renderer);
-		plot.setOrientation(PlotOrientation.VERTICAL);
+        NumberAxis domainAxis = new NumberAxis(categoryAxisLabel);
+        domainAxis.setAutoRangeIncludesZero(false);
 
-		JFreeChart chart = new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, plot, true);
+        ValueAxis valueAxis = new NumberAxis(valueAxisLabel);
 
-		return chart;
-	}
+        XYBarRenderer renderer = new ClusteredXYBarRenderer();
 
-	private static final long serialVersionUID = 1L;
-	public ChartMaker(String chartTitle)
-	{
-		super("Hashtable Chart");
-		JFreeChart barChart = createClusteredChart(
-				chartTitle,
-				"Index",
-				"Count",
-				createDataset());
+        XYPlot plot = new XYPlot(dataset, domainAxis, valueAxis, renderer);
+        plot.setOrientation(PlotOrientation.VERTICAL);
 
-		ChartPanel chartPanel = new ChartPanel(barChart);
-		chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
-		setContentPane(chartPanel);
-		XYPlot plot = (XYPlot) barChart.getPlot();
-		XYBarRenderer renderer = (XYBarRenderer) plot.getRenderer();
-	    renderer.setShadowVisible(false);
-	}
-	private IntervalXYDataset createDataset()
-	{
-		String bad = "bad hash";
-		String simple = "simple hash";
-		String rand = "random table hash";
-		String file = "text/dickens.txt";
-		XYSeriesCollection dataset = new XYSeriesCollection();
-		
-		int[] stats;
-		try (Scanner s = new Scanner(new File(file))) {
-			HashTable h = new BadTable(256);
-			h.buildTable(s);
-			stats = h.getStats();
-	        XYSeries series = new XYSeries(bad);
-			for (int i = 0; i < stats.length; i++)
-				series.add(i, stats[i]);
-			dataset.addSeries(series);
-		} catch (FileNotFoundException e) {
-			System.err.println("Cannot open file " + file + ". Exiting.");
-			System.exit(0);
-		}
-		try (Scanner s = new Scanner(new File(file))) {
-			HashTable h = new SimpleTable(256);
-			h.buildTable(s);
-			stats = h.getStats();
-	        XYSeries series = new XYSeries(simple);
-			for (int i = 0; i < stats.length; i++)
-				series.add(i, stats[i]);
-			dataset.addSeries(series);
-		} catch (FileNotFoundException e) {
-		}
-		try (Scanner s = new Scanner(new File(file))) {
-			HashTable h = new RandTable(256);
-			h.buildTable(s);
-			stats = h.getStats();
-	        XYSeries series = new XYSeries(rand);
-			for (int i = 0; i < stats.length; i++)
-				series.add(i, stats[i]);
-			dataset.addSeries(series);
-		} catch (FileNotFoundException e) {
-		}
+        JFreeChart chart = new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, plot, true);
 
-		return dataset;
-	}
-	
-	public static void main(String[] args)
-	{
-		ChartMaker chart = new ChartMaker("Distribution of hash table elements");
-		chart.pack();
-		RefineryUtilities.centerFrameOnScreen(chart);
-		chart.setVisible(true);
-	}
+        return chart;
+    }
+
+    public static void main(String[] args) {
+        ChartMaker chart = new ChartMaker("Distribution of hash table elements");
+        chart.pack();
+        RefineryUtilities.centerFrameOnScreen(chart);
+        chart.setVisible(true);
+    }
+
+    private IntervalXYDataset createDataset() {
+        String bad = "bad hash";
+        String simple = "simple hash";
+        String rand = "random table hash";
+        String file = "text/dickens.txt";
+        XYSeriesCollection dataset = new XYSeriesCollection();
+
+        int[] stats;
+        try (Scanner s = new Scanner(new File(file))) {
+            HashTable h = new BadTable(256);
+            h.buildTable(s);
+            stats = h.getStats();
+            XYSeries series = new XYSeries(bad);
+            for (int i = 0; i < stats.length; i++)
+                series.add(i, stats[i]);
+            dataset.addSeries(series);
+        } catch (FileNotFoundException e) {
+            System.err.println("Cannot open file " + file + ". Exiting.");
+            System.exit(0);
+        }
+        try (Scanner s = new Scanner(new File(file))) {
+            HashTable h = new SimpleTable(256);
+            h.buildTable(s);
+            stats = h.getStats();
+            XYSeries series = new XYSeries(simple);
+            for (int i = 0; i < stats.length; i++)
+                series.add(i, stats[i]);
+            dataset.addSeries(series);
+        } catch (FileNotFoundException e) {
+        }
+        try (Scanner s = new Scanner(new File(file))) {
+            HashTable h = new RandTable(256);
+            h.buildTable(s);
+            stats = h.getStats();
+            XYSeries series = new XYSeries(rand);
+            for (int i = 0; i < stats.length; i++)
+                series.add(i, stats[i]);
+            dataset.addSeries(series);
+        } catch (FileNotFoundException e) {
+        }
+
+        return dataset;
+    }
 }
